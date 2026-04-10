7d495d13df50b5c1d266e4e0d42707856dc10eff
ariq <ariqkhairullah000@gmail.com>
2026-04-10 22:58:04 +0700

    Added the files

---
+++ calculator_django/static/main.js
@@ -0,0 +1,50 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+const historyList = document.getElementById("historyList");
+
+let history = [];
+let currentInput = "";
+
+function addToHistory(text, result) {
+  history.push(text);
+
+  const li = document.createElement("li");
+  li.textContent = text;
+
+  li.addEventListener("click", () => {
+    currentInput = result.toString();
+    display.textContent = currentInput;
+  });
+
+  historyList.appendChild(li);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    if (value === "=") {
+      try {
+        const result = eval(currentInput);
+
+        addToHistory(currentInput + " = " + result, result);
+        g;
+        display.textContent = result;
+        currentInput = result.toString();
+      } catch {
+        display.textContent = "Error";
+        currentInput = "";
+      }
+    } else if (value === "C") {
+      currentInput = "";
+      display.textContent = "0";
+    } else if (value === "←") {
+      currentInput = currentInput.slice(0, -1);
+
+      display.textContent = currentInput || "0";
+    } else {
+      currentInput += value;
+      display.textContent = currentInput;
+    }
+  });
+});
